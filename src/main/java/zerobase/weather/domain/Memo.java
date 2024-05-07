c7d9ec27e36cc9c915e21563bd4a6ded12ace90b
soohomode <soohomode@gmail.com>
2024-05-07 21:24:45 +0900

    240507 jdbc vs jpa

--- src/main/java/zerobase/weather/domain/Memo.java
+++ src/main/java/zerobase/weather/domain/Memo.java
@@ -5,11 +5,19 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity(name = "memo")
 public class Memo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // IDENTITY 는 기본적인 키 생성을 데이터베이스에게 맡기겠다라는 키다.
     private int id;
     private String text;
 }
